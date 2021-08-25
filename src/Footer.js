3c4dc956b877f61ea009838c9aa7056b28977359
Mateo Sam <sammateo4@gmail.com>
2021-08-25 10:09:45 -0400

    Github icon added

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 import ig from './ig.png';
 import linkin from './in.png';
+import github from './github.png';
 
 function Footer(){
     return(
@@ -13,9 +14,10 @@ function Footer(){
             </div>
 
             <div>
-                <p>Created by Mateo Sam</p>
+                <p>Website created by <a className="sitelink" href="https://mateosam.netlify.app/">Mateo Sam</a></p>
                 <a href="https://www.instagram.com/sammateo4/" rel="noreferrer" target="_blank"><img className="ig-icon" src={ig} alt="instagram"></img></a>
-                <a href="https://www.linkedin.com/in/mateo-sam-464ab81b2/" rel="noreferrer" target="_blank"><img className="ig-icon" src={linkin} alt="instagram"></img></a>
+                <a href="https://www.linkedin.com/in/mateo-sam-464ab81b2/" rel="noreferrer" target="_blank"><img className="ig-icon" src={linkin} alt="linkedin"></img></a>
+                <a href="https://github.com/sammateo" rel="noreferrer" target="_blank"><img className="ig-icon" src={github} alt="github"></img></a>
             </div>
             
         </div>
